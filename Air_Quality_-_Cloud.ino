1ce4735c3775a7032fd4a59f6282e260ee023cc8
Wade753 <100038607+Wade753@users.noreply.github.com>
2022-04-20 14:22:44 +0100

    Add files via upload

--- Air_Quality_-_Cloud.ino
+++ Air_Quality_-_Cloud.ino
@@ -1,24 +1,31 @@
 #include <TinyGPS++.h>
+
 #include <SoftwareSerial.h>
+
 #include "ThingSpeak.h"
+
 #include <ESP8266WiFi.h>
+
 #include "MQ135.h"
-static const int RXPin = 4, TXPin = 5;
-static const uint32_t GPSBaud = 9600;
-float latitude , longitude;
-String  lat_str , lng_str;
+
+static
+const int RXPin = 4,
+  TXPin = 5;
+static
+const uint32_t GPSBaud = 9600;
+float latitude, longitude;
+String lat_str, lng_str;
 // repace your wifi username and password
-const char* ssid     = "VM4852933";
-const char* password = "tv4hwVdpSjk6";
+const char * ssid = "VM4852933";
+const char * password = "tv4hwVdpSjk6";
 unsigned long myChannelNumber = 1543926;
 const char * myWriteAPIKey = "HFTAQCPXIBVTQOH0";
 // The TinyGPS++ object
 TinyGPSPlus gps;
-WiFiClient  client;
+WiFiClient client;
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   ss.begin(GPSBaud);
   Serial.print("Connecting to ");
@@ -28,8 +35,8 @@ void setup()
     delay(500);
     Serial.print(".");
   }
-   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("");
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.print("Netmask: ");
@@ -38,53 +45,49 @@ void setup()
   Serial.println(WiFi.gatewayIP());
   ThingSpeak.begin(client);
 }
-void loop()
-{
-  MQ135 gasSensor = MQ135(A0);
+void loop() {
+    MQ135 gasSensor = MQ135(A0);
     float air_quality = gasSensor.getPPM();
-    Serial.print("Air Quality: ");  
+    Serial.print("Air Quality: ");
     Serial.print(air_quality);
-    Serial.println("  PPM");   
+    Serial.println("  PPM");
     Serial.println();
- 
-if (client.connect("api.thingspeak.com", 80)) // "184.106.153.149" or api.thingspeak.com
-{
-String postStr = apiKey;
-postStr += "&field1=";
-postStr += String(air_quality);
-postStr += "r\n";
-client.print("POST /update HTTP/1.1\n");
-client.print("Host: api.thingspeak.com\n");
-client.print("Connection: close\n");
-client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
-client.print("Content-Type: application/x-www-form-urlencoded\n");
-client.print("Content-Length: ");
-client.print(postStr.length());
-client.print("\n\n");
-client.print(postStr);
 
-Serial.println("Data Send to Thingspeak");
-
-
-  while (ss.available() > 0){
-    if (gps.encode(ss.read()))
+    if (client.connect("api.thingspeak.com", 80)) // "184.106.153.149" or api.thingspeak.com
     {
-      if (gps.location.isValid())
-      {
-        latitude = gps.location.lat();
-        lat_str = String(latitude , 6);
-        longitude = gps.location.lng();
-        lng_str = String(longitude , 6);
-        Serial.print("Latitude = ");
-        Serial.println(lat_str);
-        Serial.print("Longitude = ");
-        Serial.println(lng_str);
-        ThingSpeak.setField(2, lat_str);
-        ThingSpeak.setField(3, lng_str);
-        ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey); 
+      String postStr = apiKey;
+      postStr += "&field1=";
+      postStr += String(air_quality);
+      postStr += "r\n";
+      client.print("POST /update HTTP/1.1\n");
+      client.print("Host: api.thingspeak.com\n");
+      client.print("Connection: close\n");
+      client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+      client.print("Content-Type: application/x-www-form-urlencoded\n");
+      client.print("Content-Length: ");
+      client.print(postStr.length());
+      client.print("\n\n");
+      client.print(postStr);
+
+      Serial.println("Data Send to Thingspeak");
+
+      while (ss.available() > 0) {
+        if (gps.encode(ss.read())) {
+          if (gps.location.isValid()) {
+            latitude = gps.location.lat();
+            lat_str = String(latitude, 6);
+            longitude = gps.location.lng();
+            lng_str = String(longitude, 6);
+            Serial.print("Latitude = ");
+            Serial.println(lat_str);
+            Serial.print("Longitude = ");
+            Serial.println(lng_str);
+            ThingSpeak.setField(2, lat_str);
+            ThingSpeak.setField(3, lng_str);
+            ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+          }
+          delay(1000);
+          Serial.println();
+        }
       }
-     delay(1000);
-     Serial.println();  
     }
-  }
-}
